e20a8f93227f318eb1523b82868c9520b3a08c18
Rohith M <macharlarohith111@gmail.com>
2025-09-06 18:42:10 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -27,6 +27,20 @@ const nextConfig: NextConfig = {
   devIndicators: {
     allowedDevOrigins: ['https://*.cloudworkstations.dev'],
   },
+   webpack: (config, { isServer }) => {
+    // This is to fix a bug with handlebars and webpack
+    // See: https://github.com/webpack/webpack/issues/17263
+    config.module.rules.push({
+      test: /node_modules\/handlebars\/lib\/index\.js$/,
+      loader: 'string-replace-loader',
+      options: {
+        search: "require.extensions['.hbs'] = function (module, filename) {",
+        replace:
+          "require.extensions['.hbs'] = function (module, filename) { return; ",
+      },
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
